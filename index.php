5e7b69723f09937837766d68055dfb9a393ef351
eunice <eunice29548164@gmail.com>
2019-08-23 00:54:58 -0700

    ???

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$Link = new mysqli($server, $username, $password, $db);
+$Link = new mysqli("us-cdbr-iron-east-02.cleardb.net", "bf54940f57b6d8", "eabd5f32", "heroku_c89901fa5cd0d96");
 
 if(!$Link){
 	echo "連接失敗";
